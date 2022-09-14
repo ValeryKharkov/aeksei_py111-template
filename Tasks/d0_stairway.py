09c59228eea8ee783c40b358c4a20344fc517196
ValeryKharkov <kharkov.valery@gmail.com>
2022-09-14 20:38:52 +0300

    практика от 14.09.2022

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,4 +9,28 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
     print(stairway)
-    return 0
+    cost = stairway.copy()
+    for i in range(2, len(stairway)):
+        cost[i] += min(cost[1 - 1], cost[i - 2])
+    return cost[-1]
+
+def stairway_path_rec(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    cost = [-1] * len(stairway)
+    cost[0] = stairway[0]
+    cost[1] = stairway[1]
+
+    def rec(n: int) -> Union[float, int]:
+        if cost[n] != '':
+            return cost[n]
+
+        if n < 2:
+            return cost[n]
+
+        cost[n] = min(rec(n - 1) + stairway[n], rec(n-2) + stairway[n])
+        return cost[n]
+    return rec(len(stairway) - 1)
+
+list_ = [1, 3, 1, 5, 2, 7, 7, 8, 9, 4, 6, 3]
+
+print(stairway_path(list_))
+print(stairway_path_rec(list_))
--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -7,4 +7,20 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
     print(shape, point)
-    return 0
+    table = [[0 for i in range(shape[1])] for i in range(shape[0])]
+    table[start_point[0]][start_point[1]] = 1
+
+    def path_rec(i, j):
+        if 0 <= i <= shape[0] - 1 and 0 <= j <= shape[1] - 1:
+            if table[i][j]:
+                return table[i][j]
+            table[i][j] = sum((path_rec(i + 1, j - 2),
+                               path_rec(i - 1, j - 2),
+                               path_rec(i - 2, j - 1),
+                               path_rec(i - 2, j + 1)
+                              ))
+            return table[i][j]
+
+
+        return 0
+    return path_rec(*point)
